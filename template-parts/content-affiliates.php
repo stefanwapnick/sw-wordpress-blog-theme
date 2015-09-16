4182f0a812edd7d92e624e94bfe05176e07df1e1
stefanwapnick <stefanwapnick@hotmail.com>
2015-09-16 17:48:56 -0400

    Fixed bug in displaying affiliates page

--- template-parts/content-affiliates.php
+++ template-parts/content-affiliates.php
@@ -11,7 +11,7 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
 ================================================== -->
 
 	<?php if(has_post_thumbnail()) : ?>
-		<section class="feature-image feature-image-default" style="background: url('<?= $thumbnail_url ?>') no-repeat; background-size: cover" data-type="parallax" data-speed-factor="4">
+		<section class="feature-image feature-image-default" style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover" data-type="parallax" data-speed-factor="4">
 			<h1 class="page-title"><i class="fa fa-cog"></i> Affiliates</h1>
 		</section>
 	<?php else: ?>
@@ -34,18 +34,18 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
 
 				<?php $loop = new WP_Query( array('post_type' => 'affiliate', 'orderby', 'post_id', 'order' => 'ASC')); ?>
 
-					<?php while($loop->have_posts()) : $loop -> the_post() ?>
+					<?php while($loop->have_posts()) : $loop -> the_post(); ?>
 
 						<div class="col-sm-4">
 
 							<?php get_section_title_image(get_field('affiliate_image')); ?>
 
-							<h3><a href="<?php the_field('affiliate_url') ?>"> <?php the_title(); ?></a></h3>
+							<h3><a href="<?php the_field('affiliate_url'); ?>"> <?php the_title(); ?></a></h3>
 
 							<p><?php the_content(); ?></p>
 
 							<?php if(!empty(get_field('affiliate_button'))) : ?>
-								<a href="<?php the_field('affiliate_url') ?>" class="btn btn-danger btn-block"> <?php the_field('affiliate_button'); ?> </a>
+								<a href="<?php the_field('affiliate_url'); ?>" class="btn btn-danger btn-block"> <?php the_field('affiliate_button'); ?> </a>
 							<?php endif; ?>
 
 						</div>
